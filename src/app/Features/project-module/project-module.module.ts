244ef398451f2375427742f8d0ab53050295e4bb
Anindita Roy <ranindita231@gmail.com>
2025-01-16 18:21:39 +0600

    search bar

--- src/app/Features/project-module/project-module.module.ts
+++ src/app/Features/project-module/project-module.module.ts
@@ -5,12 +5,12 @@ import { ProjectModuleRoutingModule } from './project-module-routing.module';
 import { ProjectCreateComponent } from './pages/project-create/project-create.component';
 import { ProjectListComponent } from './pages/Project-list/Project-list.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { ProjectAssignComponent } from './pages/project-assign/project-assign.component';
+
 import { ProjectDetailComponent } from './pages/project-detail/project-detail.component';
 
 
 @NgModule({
-  declarations: [ProjectCreateComponent,ProjectListComponent,ProjectDetailComponent,ProjectAssignComponent],
+  declarations: [ProjectCreateComponent,ProjectListComponent,ProjectDetailComponent],
   imports: [
     CommonModule,
     ProjectModuleRoutingModule,
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'user/user-list', pathMatch: 'full' },
+  
   {
     path: 'user',
     loadChildren: () =>
