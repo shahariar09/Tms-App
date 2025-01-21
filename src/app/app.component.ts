059a8549a7af1be6b746faf89c49d01764d7289e
Anindita Roy <ranindita231@gmail.com>
2025-01-21 15:40:03 +0600

    logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { Component } from '@angular/core'; 
 import { CommonModule } from '@angular/common'; 
+import { LoginService } from './Features/login/services/login.service';
 
 
 @Component({
@@ -15,7 +16,7 @@ export class AppComponent {
   projectMenuOpen: boolean = false;;
   taskMenuOpen: boolean = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private loginService: LoginService) {}
 
   toggleUserMenu() {
     this.userMenuOpen = !this.userMenuOpen;
@@ -38,6 +39,10 @@ export class AppComponent {
   }
   isChangePasswordPage(): boolean {
     return this.router.url.includes('change-password');
+  }
+
+  logout() {
+    this.loginService.logout(); // Call the logout method from LoginService
   }
   
 }
